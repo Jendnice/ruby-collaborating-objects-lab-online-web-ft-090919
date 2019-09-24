4b5426965f5fd24e29deb704dc68c66dfc54bb18
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-24 14:11:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
    # self.artist.add_song(self)
-  #   self.artist.name
+     self.artist.name
   end 
 
   
